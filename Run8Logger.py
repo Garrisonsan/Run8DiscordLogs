38d0e01d8036be32e09132723d695b69c6d02b8e
Garrisonsan <50163244+Garrisonsan@users.noreply.github.com>
2026-04-02 20:08:38 -0500

    Add more license info from referenced upstream projects

--- Run8Logger.py
+++ Run8Logger.py
@@ -1,9 +1,32 @@
 #! python3
 
 """
-Tail implementation from https://pypi.org/project/pygtail/
+Tail implementation from https://pypi.org/project/pygtail/ (license is the cancerous GPLv2, included as a separate file)
 Discord.py library from https://github.com/Rapptz/discord.py
 Basis for Discord communication from https://github.com/Rapptz/discord.py/blob/v2.1.1/examples/background_task_asyncio.py
+
+License for discord.py and background_task_asyncio.py is as follows:
+The MIT License (MIT)
+
+Copyright (c) 2015-present Rapptz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
 """
 
 import argparse
